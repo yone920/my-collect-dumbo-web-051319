5e71f4038dcc815ae730215db5742f5f26b07810
Yonas Fesehatsion <yone920@gmail.com>
2019-04-17 20:52:39 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,9 +4,12 @@ def my_collect(array)
   new_array = []
   i = 0
     while i < array.length
-    new_array.push(yield i)
+    new_array.push(yield(array[i]))
+    
       i += 1 
     end
+    
  new_array
+ binding.pry
 end
 
